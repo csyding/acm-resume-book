c290c6ff6ab383ba1273bfa671fe51a0a71e635f
Scott Kim <scottk3@illinois.edu>
2020-03-23 14:58:59 -0500

    Remove accidentally added sqlite/postgres

--- acm_resume_book/settings.py
+++ acm_resume_book/settings.py
@@ -78,10 +78,7 @@ WSGI_APPLICATION = 'acm_resume_book.wsgi.application'
 mongo_uri = urlparse.urlparse(os.environ['MONGODB_URI'])
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
+    'default': {},
     'ourgraphdb': {
         'ENGINE': 'djongo',
         'NAME': 'heroku_l4jz1453',
