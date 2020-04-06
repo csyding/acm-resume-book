ed58a01f532aeada1725c1e256223cb032a8c9f2
Scott Kim <scottk3@illinois.edu>
2020-04-07 03:30:26 +0900

    Add a barebones StudentView page with create/view/update/delete just to have reference on how to do this

--- resume_book/models.py
+++ resume_book/models.py
@@ -10,3 +10,10 @@ class Student(models.Model):
         return "Netid is: " + str(self.netID)
 
 
+class StudentGroup(models.Model):
+    name = models.CharField(max_length=20, primary_key=True)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return "Student Group: " + str(self.name)
+
