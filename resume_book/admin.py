f56af98a1768c8b2d9dd3f938cc397a993259229
Melissa Chen <myc2@illinois.edu>
2020-04-06 18:28:55 -0500

    Fixing build failure for admin

--- resume_book/admin.py
+++ resume_book/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student, StudentGroup
+from .models import Student, StudentGroup, Resume, Internship, Company, Recruiter
 
 admin.site.register(Student)
 admin.site.register(StudentGroup)
